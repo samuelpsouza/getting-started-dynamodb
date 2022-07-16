9d32bb1db4378fbd633f67de6c98706121fa59ac
Samuel Souza <samuelsouza.contact@gmail.com>
2022-07-16 09:08:34 -0300

    first commit

---
+++ src/test/kotlin/dev/ssouza/gettingstarteddynamodb/GettingStartedDynamodbApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.ssouza.gettingstarteddynamodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GettingStartedDynamodbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
